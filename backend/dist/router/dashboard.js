fd1c43519f3ba95305107d7088aff36f7be1fe91
Nirmal Bhandari <bhdnirmal99@gmail.com>
2024-01-24 13:38:24 +0000

    removed dist from being tracked

--- backend/dist/router/dashboard.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dashboard_1 = require("../controllers/dashboard");
-const middlewares_1 = require("../middlewares");
-exports.default = (router) => {
-    router.get("/get-user-report-data", middlewares_1.isAuthenticated, dashboard_1.getUserLogData);
-};
-//# sourceMappingURL=dashboard.js.map
