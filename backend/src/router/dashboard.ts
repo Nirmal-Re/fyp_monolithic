2454beadef25f539b995f9979085736343d27b5e
Nirmal Bhandari <bhdnirmal99@gmail.com>
2024-01-26 14:35:35 +0000

    changed get-user-report-data from get to post

--- backend/src/router/dashboard.ts
+++ backend/src/router/dashboard.ts
@@ -7,7 +7,7 @@ import {
 import { isAuthenticated } from "../middlewares";
 
 export default (router: Router) => {
-  router.get("/get-user-report-data", isAuthenticated, getUserLogData);
+  router.post("/get-user-report-data", isAuthenticated, getUserLogData);
   router.post(
     "/get-workout-historic-data",
     isAuthenticated,
