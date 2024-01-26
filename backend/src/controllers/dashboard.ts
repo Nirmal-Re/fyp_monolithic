bd3b02f659c76d953d9824b6c04e2f6608708511
Nirmal Bhandari <bhdnirmal99@gmail.com>
2024-01-26 14:17:11 +0000

    added date and time filter to getUserLogData

--- backend/src/controllers/dashboard.ts
+++ backend/src/controllers/dashboard.ts
@@ -6,11 +6,18 @@ import { getHistoryWorkoutData } from "../model/exercise";
 
 export const getUserLogData = async (req: Request, res: Response) => {
   try {
-    const { uid } = req.body;
-    const stats = await getHabitStats(uid);
+    const { uid, start, end } = req.body;
+    const [startDate, endDate] = convertDates(start, end);
+    if (!validDates(startDate, endDate)) {
+      console.log(startDate, endDate);
+      return res.status(400).send({ error: "Invalid dates" });
+    }
+    const stats = await getHabitStats(uid, startDate, endDate);
     return res.status(200).send(stats);
-  } catch (e) {
-    res.status(400).send({ error: "Error with getting historic log data" });
+  } catch (e: unknown) {
+    console.error(e);
+    if (e instanceof Error) res.status(400).send({ error: e.message });
+    else res.status(400).send({ error: "Error with getting historic data" });
   }
 };
 
--- backend/src/model/logs.ts
+++ backend/src/model/logs.ts
@@ -99,9 +99,14 @@ export const deleteLog = async (id: string) => {
   return await m_deleteOne("coll_logs", { id });
 };
 
-export const getHabitStats = async (userId: string): Promise<any[]> => {
+export const getHabitStats = async (userId: string, start: Date, end: Date) => {
   const pipeline = [
-    { $match: { uid: Number(userId) } },
+    {
+      $match: {
+        uid: Number(userId),
+        uploadDateAndTime: { $gte: start, $lte: end },
+      },
+    },
     { $sort: { uploadDateAndTime: 1 } },
     {
       $project: {
